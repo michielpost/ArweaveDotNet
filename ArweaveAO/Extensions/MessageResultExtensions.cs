67203b63422c0470aec334b442fb3f213d2c2bef
Michiel Post <michiel@michielpost.nl>
2024-06-19 14:16:06 +0200

    Added extension method

---
+++ ArweaveAO/Extensions/MessageResultExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ArweaveAO.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArweaveAO.Extensions
+{
+    public static class MessageResultExtensions
+    {
+        public static string? GetFirstTagValue(this MessageResult messageResult, string tag)
+        {
+            return messageResult.Messages.FirstOrDefault()?.Tags.Where(x => x.Name == "Name").Select(x => x.Value).FirstOrDefault();
+        }
+    }
+}
--- ArweaveAO/TokenClient.cs
+++ ArweaveAO/TokenClient.cs
@@ -1,4 +1,5 @@
-﻿using ArweaveAO.Models;
+﻿using ArweaveAO.Extensions;
+using ArweaveAO.Models;
 using ArweaveAO.Models.Token;
 using ArweaveAO.Requests;
 using Microsoft.Extensions.Options;
@@ -38,12 +39,12 @@ namespace ArweaveAO
 
                 var tokenData = new TokenData();
                 tokenData.TokenId = processId;
-                tokenData.Name = result.Messages.First().Tags.Where(x => x.Name == "Name").Select(x => x.Value).FirstOrDefault();
-                tokenData.Ticker = result.Messages.First().Tags.Where(x => x.Name == "Ticker").Select(x => x.Value).FirstOrDefault();
-                tokenData.Logo = result.Messages.First().Tags.Where(x => x.Name == "Logo").Select(x => x.Value).FirstOrDefault();
+                tokenData.Name = result.GetFirstTagValue("Name");
+                tokenData.Ticker = result.GetFirstTagValue("Ticker");
+                tokenData.Logo = result.GetFirstTagValue("Logo");
                 
                 
-                string? denomination = result.Messages.First().Tags.Where(x => x.Name == "Denomination").Select(x => x.Value).FirstOrDefault();
+                string? denomination = result.GetFirstTagValue("Denomination");
                 if(!string.IsNullOrWhiteSpace(denomination) && int.TryParse(denomination, out int denominationInt))
                     tokenData.Denomination = denominationInt;
 
@@ -82,10 +83,10 @@ namespace ArweaveAO
 
                 var balanceData = new BalanceData();
                 balanceData.TokenId = tokenId;
-                balanceData.Ticker = result.Messages.First().Tags.Where(x => x.Name == "Ticker").Select(x => x.Value).FirstOrDefault();
-                balanceData.Account = result.Messages.First().Tags.Where(x => x.Name == "Account").Select(x => x.Value).FirstOrDefault();
+                balanceData.Ticker = result.GetFirstTagValue("Ticker");
+                balanceData.Account = result.GetFirstTagValue("Account");
                 
-                string? balance = result.Messages.First().Tags.Where(x => x.Name == "Balance").Select(x => x.Value).FirstOrDefault();
+                string? balance = result.GetFirstTagValue("Balance");
                 if (!string.IsNullOrWhiteSpace(balance) && long.TryParse(balance, out long balanceLong))
                     balanceData.Balance = balanceLong;
 
